ced6f3aa3b255b505634a11d1077dc3d8b76e43f
Martijn Bentum <bentummartijn@gmail.com>
2022-02-07 14:21:27 +0100

    added Helper class with extra properties for Installation model

--- utils/model_util.py
+++ utils/model_util.py
@@ -34,9 +34,85 @@ class info():
 
 	
 
+class Helper:
+	@property
+	def identifier(self):
+		app_name, model_name = instance2names(self)
+		return '_'.join([app_name,model_name,str(self.pk)]).lower()
+
+	@property
+	def detail_url(self):
+		app_name, model_name = instance2names(self)
+		return app_name + ':' + model_name.lower()
+
+	@property
+	def edit_url(self):
+		app_name, model_name = instance2names(self)
+		return app_name + ':' + model_name.lower() + ':new' 
+
+	@property
+	def start_date_str(self):
+		if self.construction_date_lower and self.construction_date_upper:
+			if self.construction_date_lower == self.construction_date_upper:
+				return str(self.construction_date_lower.year)
+			ly = str(self.construction_date_lower.year)
+			uy = str(self.construction_date_upper.year)
+			return '(' + ly + ' ' + uy + ')'
+		if self.construction_date_lower:
+			return str(self.construction_date_lower.year)
+		if self.construction_date_upper:
+			return str(self.construction_date_upper.year)
+		return ''
+
+	@property
+	def end_date_str(self):
+		if self.end_functioning_year_lower and self.end_functioning_year_upper:
+			if self.end_functioning_year_lower == self.end_functioning_year_upper:
+				return str(self.end_functioning_year_upper.year)
+			ly = str(self.end_functioning_year_lower.year)
+			uy = str(self.end_functioning_year_upper.year)
+			return '(' + ly + ' ' + uy + ')'
+		if self.end_functioning_year_lower:
+			return str(self.end_functioning_year_lower.year)
+		if self.end_functioning_year_upper:
+			return str(self.end_functioning_year_upper.year)
+		return ''
+
+	@property
+	def start_date(self):
+		if self.construction_date_lower: 
+			return self.construction_date_lower
+		if self.construction_date_upper:
+			return self.construction_date_upper
+		return None
+		
+	@property
+	def end_date(self):
+		if self.end_functioning_year_upper: 
+			return self.end_functioning_year_upper
+		if self.end_functioning_year_lower:
+			return self.end_functioning_year_lower
+		return None
+
+
+	@property
+	def has_exact_location(self):
+		return bool(self.extent_shapefile and self.latitude and self.longitude)
+
+
 
 def id_generator(id_type= 'letters', length = 9):
 	if id_type == 'letters':
 		return ''.join(random.sample(string.ascii_letters*length,length))
 	if id_type == 'numbers':
 		return int(''.join(random.sample('123456789'*length,length)))
+
+
+def instance2names(instance):
+    app_name = instance._meta.app_label 
+    model_name = instance._meta.model_name.capitalize()
+    return app_name, model_name
+
+def instance2name(instance):
+    app_name, model_name = instance2names(instance)
+    return model_name
