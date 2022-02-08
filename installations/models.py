dc537aa74a96c5c90b312d58dace9e0940e175da
Martijn Bentum <bentummartijn@gmail.com>
2022-02-08 14:59:19 +0100

    refactoring is hard; moved style and figure back to installations.models

--- installations/models.py
+++ installations/models.py
@@ -36,6 +36,19 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+
+class Style(models.Model):
+    name = models.CharField(max_length=200)
+    color = ColorField(default='#FF0000')
+    stroke_opacity = models.FloatField(default=0.8, )
+    stroke_weight = models.IntegerField(default=2)
+    fill_opacity = models.FloatField(default=0.3)
+    dashed = models.BooleanField(default=False)
+    z_index = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name + ' ' + self.color
+
 class Neighbourhood(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE, blank=False, 
         null=False, default='')
@@ -48,6 +61,20 @@ class Neighbourhood(models.Model):
     def __str__(self):
         st = self.city.name + ' ' + str(self.neighbourhood_number)
         return st
+
+class Figure(models.Model):
+    '''figure to be plotted on a map.'''
+    name = models.CharField(max_length=200)
+    style = models.ForeignKey(Style,  on_delete=models.SET_NULL, blank=False, 
+        null=True, default='')
+    start_date = PartialDateField(null=True, blank=True)
+    end_date = PartialDateField(null=True, blank=True)
+    geojson = models.FileField(upload_to='shapefiles/', null=False, 
+        blank=False, default='')
+    neighbourhood = models.ManyToManyField(Neighbourhood, blank=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, blank=False, 
+        null=False, default='')
+    description = models.CharField(max_length=1000, blank=True, default='', null=True)
 # Landmarks
 
 
