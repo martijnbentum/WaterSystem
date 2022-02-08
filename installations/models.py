7c5f473a84b25efc4992fa8717d77680d81a5d50
Martijn Bentum <bentummartijn@gmail.com>
2022-02-08 13:54:48 +0100

    added figure, style, neighbourhood from installations.models

--- installations/models.py
+++ installations/models.py
@@ -36,69 +36,7 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
-
-class CityEra(models.Model):
-    name = models.CharField(max_length=100, blank=False)
-    city = models.ForeignKey(City, on_delete=models.CASCADE, blank=True)
-    start_date = PartialDateField()
-    end_date = PartialDateField()
-    population = models.IntegerField()
-
-    def __str__(self):
-        return self.name
-
-
-class Citymap(models.Model):
-    # map = models.ImageField()
-    type = models.CharField(max_length=50, blank=False)
-    city = models.ForeignKey(City, related_name='citymaps', 
-        on_delete=models.CASCADE, blank=True)
-    # I think we do nat need population here, as we have it in CityEra
-    population = models.IntegerField()  
-    start_date = PartialDateField()
-    end_date = PartialDateField()
-
-
 # Landmarks
-class Style(models.Model):
-    name = models.CharField(max_length=200)
-    color = ColorField(default='#FF0000')
-    stroke_opacity = models.FloatField(default=0.8, )
-    stroke_weight = models.IntegerField(default=2)
-    fill_opacity = models.FloatField(default=0.3)
-    dashed = models.BooleanField(default=False)
-    z_index = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.name + ' ' + self.color
-
-
-class Neighbourhood(models.Model):
-    city = models.ForeignKey(City, on_delete=models.CASCADE, blank=False, 
-        null=False, default='')
-    neighbourhood_number = models.PositiveIntegerField(null=True, blank=True)
-    style = models.ForeignKey(Style, on_delete=models.CASCADE, blank=True, 
-        null=True, default=None)
-    extent_shapefile = models.FileField(upload_to='shapefiles/', max_length=50, 
-        null=True,blank=True)  # Is it correct way?
-
-    def __str__(self):
-        st = self.city.name + ' ' + str(self.neighbourhood_number)
-        return st
-
-class Figure(models.Model):
-    '''figure to be plotted on a map.'''
-    name = models.CharField(max_length=200)
-    style = models.ForeignKey(Style,  on_delete=models.CASCADE, blank=False, 
-        null=False, default='')
-    start_date = PartialDateField(null=True, blank=True)
-    end_date = PartialDateField(null=True, blank=True)
-    geojson = models.FileField(upload_to='shapefiles/', null=False, 
-        blank=False, default='')
-    neighbourhood = models.ManyToManyField(Neighbourhood, blank=True)
-    city = models.ForeignKey(City, on_delete=models.CASCADE, blank=False, 
-        null=False, default='')
-    description = models.CharField(max_length=1000, blank=True, default='', null=True)
 
 
 class Religion(models.Model):
--- map/models.py
+++ map/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from installations.models import City
 
 # Create your models here.
+class Style(models.Model):
+    name = models.CharField(max_length=200)
+    color = ColorField(default='#FF0000')
+    stroke_opacity = models.FloatField(default=0.8, )
+    stroke_weight = models.IntegerField(default=2)
+    fill_opacity = models.FloatField(default=0.3)
+    dashed = models.BooleanField(default=False)
+    z_index = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name + ' ' + self.color
+
+
+class Neighbourhood(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE, blank=False, 
+        null=False, default='')
+    neighbourhood_number = models.PositiveIntegerField(null=True, blank=True)
+    style = models.ForeignKey(Style, on_delete=models.SET_NULL, blank=True, 
+        null=True, default=None)
+    extent_shapefile = models.FileField(upload_to='shapefiles/', max_length=50, 
+        null=True,blank=True)  # Is it correct way?
+
+    def __str__(self):
+        st = self.city.name + ' ' + str(self.neighbourhood_number)
+        return st
+
+class Figure(models.Model):
+    '''figure to be plotted on a map.'''
+    name = models.CharField(max_length=200)
+    style = models.ForeignKey(Style,  on_delete=models.SET_NULL, blank=False, 
+        null=False, default='')
+    start_date = PartialDateField(null=True, blank=True)
+    end_date = PartialDateField(null=True, blank=True)
+    geojson = models.FileField(upload_to='shapefiles/', null=False, 
+        blank=False, default='')
+    neighbourhood = models.ManyToManyField(Neighbourhood, blank=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, blank=False, 
+        null=False, default='')
+    description = models.CharField(max_length=1000, blank=True, default='', null=True)
