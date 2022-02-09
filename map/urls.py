cbe0cc3c372d9e667754f45d54071c564e674000
Martijn Bentum <bentummartijn@gmail.com>
2022-02-09 16:57:54 +0100

    add url for get_neighbourhood view

--- map/urls.py
+++ map/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('map/', views.MapVisualization, name='map-visualization'),
     path('geojson_file/shapefiles/<str:filename>/', views.geojson_file, 
         name='geojson_file'),
+    path('get_neighbourhood/<str:installation_identifier>/', views.get_neighbourhood, 
+        name='get_neighbourhood'),
 	]
