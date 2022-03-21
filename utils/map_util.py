89aff22e2ce8dc4f6b9d07fe3cadc16b13210645
Martijn Bentum <bentummartijn@gmail.com>
2022-03-21 15:29:54 +0100

    add class filters, category filter and filter to pass filter information to the map view

--- utils/map_util.py
+++ utils/map_util.py
@@ -1,5 +1,6 @@
 from installations.models import Figure, Style, City, Neighbourhood 
-from installations.models import Institution, Installation
+from installations.models import Institution, Installation, WatersystemCategories
+import json
 
 def city_to_installation_dict():
 	cities = City.objects.all()
@@ -56,3 +57,150 @@ def _get_city_name(city):
 	else: city_name = None
 	return city_name
 	
+class Filters():
+	def __init__(self):
+		self.watersystemcategories = WatersystemCategories.objects.all()
+		self.make_all()
+
+	def make_all(self):
+		self._make_category_filters()
+		self._make_installation_ids2city_dict()
+		self._make_city2installation_ids_dict()
+		
+	def _make_category_filters(self):
+		self.category_filters,self.installations = [],[]
+		self.filters,self.installation_ids = [],[]
+		for watersystem_category in self.watersystemcategories:
+			cf = CategoryFilter(watersystem_category)
+			self.filters.extend(cf.filters)
+			self.category_filters.append(cf)
+			self.installation_ids.extend(cf.installation_ids)
+			self.installations.extend(cf.installations)
+
+	def _make_installation_ids2city_dict(self):
+		self.installation_ids2city_dict = {}
+		for f in self.filters:
+			self.installation_ids2city_dict.update(f.installation_ids2city_dict)
+
+	def _make_city2installation_ids_dict(self):
+		self.city2installation_ids_dict = {}
+		for installation_id, city in self.installation_ids2city_dict.items():
+			if city not in self.city2installation_ids_dict.keys():
+				self.city2installation_ids_dict[city] = []
+			self.city2installation_ids_dict[city].append(installation_id)
+
+	def to_dict(self):
+		d = {'filters':[],'category_filters':[],'installation_ids':self.installation_ids}
+		d['filter_dict'] = {}
+		d['city2installations'] = self.city2installation_ids_dict
+		d['installations2city'] = self.installation_ids2city_dict
+		for cf in self.category_filters:
+			cfd = cf.to_dict()
+			d['category_filters'].append(cfd)
+			d['filters'].extend(cfd['filters'])
+		for f in self.filters:
+			d['filter_dict'][f.name] = f.to_dict()
+		return d
+
+
+
+
+class CategoryFilter():
+	def __init__(self, watersystem_category):
+		self.watersystem_category = watersystem_category
+		self.name = watersystem_category.name
+		self._set_all()
+
+	def __repr__(self):
+		m = self.name.ljust(15) +' | '
+		m += 'ninstallations: ' + str(len(self.installations)).ljust(3) + ' | '
+		m += 'nfilters: ' + str(len(self.filters)).ljust(3) + ' | '
+		m += ', '.join([f.name for f in self.filters]) 
+		return m
+
+	def _set_all(self):
+		self.watersystems = self.watersystem_category.watersystems
+		self._group_watersystem_types()
+		self.filters,self.installations,self.installation_ids = [], [], []
+		wc = self.watersystem_category
+		for watersystem_type, watersystem_instances in self.watersystem_types.items():
+			f = Filter(watersystem_type,watersystem_instances,wc)
+			self.filters.append(f)
+			self.installations.extend(f.installations)
+			self.installation_ids.extend(f.installation_ids)
+		self.filter_names = ','.join([f.name for f in self.filters])
+
+	def _group_watersystem_types(self):
+		self.watersystem_types = {}
+		for watersystem in self.watersystems:
+			if watersystem.type not in self.watersystem_types.keys():
+				self.watersystem_types[watersystem.type] = []
+			self.watersystem_types[watersystem.type].append(watersystem)
+
+	def to_dict(self):
+		d = {'name':self.name}
+		d['ninstallations'] = len(self.installations)
+		d['nfilters']=len(self.filters)
+		d['filter_names']=self.filter_names
+		d['installation_ids_str'] = ','.join(self.installation_ids)
+		d['filters'] = []
+		d['active'] =True
+		for f in self.filters:
+			d['filters'].append( f.to_dict() )
+		return d
+
+	def to_json(self):
+		d = self.to_dict()
+		return json.dumps(d)
+
+		
+
+class Filter():
+	def __init__(self, name, watersystem_instances, watersystem_category):
+		self.name = name.replace(',','')
+		self.watersystem_instances = watersystem_instances
+		self.watersystem_category = watersystem_category
+		self.category_name = watersystem_category.name
+		self._set_all()
+
+	def __repr__(self):
+		m = self.name.ljust(15) +' | '
+		m += 'ninstallations: ' + str(len(self.installations)).ljust(3) + ' | '
+		m += ', '.join([wi.name for wi in self.watersystem_instances]) 
+		return m
+		
+	def _set_all(self):
+		self._set_installations()
+		self._make_installation_ids2city_dict()
+
+	def _set_installations(self):
+		self.installations = []
+		self.installation_ids = []
+		for watersystem in self.watersystem_instances:
+			self.installations.extend([i for i in watersystem.installations if i.city])
+			self.installation_ids.extend(watersystem.installation_identifiers.split(','))
+
+	def _make_installation_ids2city_dict(self):
+		self.installation_ids2city_dict = {}
+		for installation in self.installations:
+			if not installation.city:continue
+			self.installation_ids2city_dict[installation.identifier] = installation.city.name
+
+	def to_dict(self):
+		d = {}
+		d['ninstallations'] = len(self.installations)
+		d['installation_ids_str'] = ','.join(self.installation_ids)
+		d['active'] =True
+		for k, value in self.__dict__.items():
+			if k in 'watersystem_category,watersystem_instances,installations':continue
+			d[k] =value
+		return d
+
+	def to_json(self):
+		d = self.to_dict()
+		return json.dumps(d)
+		
+
+			
+	
+
